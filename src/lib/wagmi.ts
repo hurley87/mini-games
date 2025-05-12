a4ed6dc95d520de8950dddbb0af47297994fe978
David Hurley <dhurls99@gmail.com>
2025-05-12 13:55:12 -0400

    add supabase and smart wallet provider

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,25 @@
+import { http, createConfig } from "wagmi";
+import { base } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
+ 
+export const cbWalletConnector = coinbaseWallet({
+  appName: "Mini Games Studio",
+  preference: "smartWalletOnly",
+});
+ 
+export const config = createConfig({
+  chains: [base],
+  // turn off injected provider discovery
+  multiInjectedProviderDiscovery: false,
+  connectors: [cbWalletConnector],
+  ssr: true,
+  transports: {
+    [base.id]: http(),
+  },
+});
+ 
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
