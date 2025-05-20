e46f40585523ae0dd68b1cdd3af73c70122c7774
David Hurley <dhurls99@gmail.com>
2025-05-20 15:48:27 -0400

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 import { Toaster } from 'sonner';
-import Header from '@/components/header';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
